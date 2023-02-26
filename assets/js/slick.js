8b9d60b64306c89c9401cfd2a20f494cb8b93cd9
Masud03Am <masudboy2203@gmail.com>
2023-02-26 12:30:57 +0500

    Rezero_V1.9

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,11 +1,13 @@
 $('.responsive').slick({
-      dots: true,
+      dots: false,
       infinite: true,
-      speed: 3000,
+      speed: 300,
       slidesToShow: 4,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 3500,
+      swipe: true,
+      touchThreshold: 5,
       responsive: [
         {
           breakpoint: 1024,
@@ -13,21 +15,23 @@ $('.responsive').slick({
             slidesToShow: 3,
             slidesToScroll: 3,
             infinite: true,
-            dots: true
+            dots: false
           }
         },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 2,
+            dots: false
           }
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            dots: false
           }
         }
         // You can unslick at a given breakpoint now by adding:
