ca03498b102a56258f784d9064e9bd7c0f5c1203
Masud03Am <masudboy2203@gmail.com>
2023-03-01 20:31:54 +0500

    Rezero_V1.24

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,6 @@
    * Preloader
    */
 
-  window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    }, 500);
-  }
 
   /**
    * Easy selector helper function
